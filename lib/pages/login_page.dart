95255fe7d9f74ef3a8305bbeee140c9c95cc0a6c
Nitin Khobragade <khobragadenitin7@gmail.com>
2022-01-06 13:55:30 +0530

    Day 4 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,19 +3,59 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key key}) : super(key: key);
 
- @override
+  @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: 
-      Text(
-        "Login Page",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold
-        ),
-        ),
-        ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_two.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter UserName", labelText: "UserName"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Passord", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Row(
+                      children: [
+                        Text("Login")
+                      ],
+                    ),
+                    onPressed: (){
+                      print("Hello Nitin");
+                    },
+                  )
+
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
